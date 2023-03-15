f8f24b52f4389fbb087af872d3bbc3be96206926
Louis Li <a0909002250@gmail.com>
2023-03-15 20:38:19 +0800

    feat: 套件版本改用kotlin-dsl方式管理

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import tw.idv.louislee.Version
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -50,19 +52,17 @@ android {
 }
 
 dependencies {
-    val compose_version = "1.3.3"
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.0")
-    implementation("androidx.activity:activity-compose:1.6.1")
-    implementation("androidx.compose.ui:ui:$compose_version")
-    implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
-    implementation("androidx.compose.material3:material3:1.1.0-alpha08")
+    implementation("androidx.core:core-ktx:${Version.ANDROID_CORE}")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${Version.LIFECYCLE}")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${Version.LIFECYCLE}")
+    implementation("androidx.activity:activity-compose:${Version.ACTIVITY_COMPOSE}")
+    implementation("androidx.compose.ui:ui:${Version.COMPOSE}")
+    implementation("androidx.compose.ui:ui-tooling-preview:${Version.COMPOSE}")
+    implementation("androidx.compose.material3:material3:${Version.MATERIAL_3}")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$compose_version")
-    debugImplementation("androidx.compose.ui:ui-tooling:$compose_version")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:$compose_version")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${Version.COMPOSE}")
+    debugImplementation("androidx.compose.ui:ui-tooling:${Version.COMPOSE}")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:${Version.COMPOSE}")
 }
---
+++ buildSrc/src/main/java/tw/idv/louislee/Version.kt
@@ -0,0 +1,9 @@
+package tw.idv.louislee
+
+object Version {
+    const val ANDROID_CORE = "1.9.0"
+    const val ACTIVITY_COMPOSE = "1.6.1"
+    const val COMPOSE = "1.3.3"
+    const val LIFECYCLE = "2.6.0"
+    const val MATERIAL_3 = "1.1.0-alpha08"
+}
