b95e49c09deecb3039e00b686f4b13822a1e0b33
Louis Li <a0909002250@gmail.com>
2023-03-18 09:17:50 +0800

    fix: getWindowInsetsController棄用的問題

--- app/src/main/java/tw/idv/louislee/accountingbook/ui/theme/Theme.kt
+++ app/src/main/java/tw/idv/louislee/accountingbook/ui/theme/Theme.kt
@@ -9,7 +9,7 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -51,8 +51,10 @@ fun AccountingBookTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            val currentWindow = (view.context as Activity).window
+            currentWindow.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(currentWindow, view)
+                .isAppearanceLightStatusBars = darkTheme
         }
     }
 
