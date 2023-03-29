8e44598113d2004f51d374e01b57c7a3d7025227
Louis Li <a0909002250@gmail.com>
2023-03-29 20:17:26 +0800

    refactor: 移除AccountingEventViewModel的interface

--- app/src/main/java/tw/idv/louislee/accountingbook/page/accountingevent/AccountingEventViewModel.kt
+++ app/src/main/java/tw/idv/louislee/accountingbook/page/accountingevent/AccountingEventViewModel.kt
@@ -2,18 +2,10 @@ package tw.idv.louislee.accountingbook.page.accountingevent
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.Flow
 import org.koin.android.annotation.KoinViewModel
-import tw.idv.louislee.accountingbook.domain.dto.AccountingEventDto
 import tw.idv.louislee.accountingbook.domain.service.AccountingEventService
 
-interface AccountingEventViewModel {
-    fun findAll(): Flow<List<AccountingEventDto>>
-}
-
 @KoinViewModel
-class AccountingEventViewModelImpl(
-    private val service: AccountingEventService
-) : ViewModel(), AccountingEventViewModel {
-    override fun findAll() = service.findAll(viewModelScope.coroutineContext)
+class AccountingEventViewModel(private val service: AccountingEventService) : ViewModel() {
+    fun findAll() = service.findAll(viewModelScope.coroutineContext)
 }
