cddc85e1f590d5a220105b7eb422d145ad635a88
Louis Li <a0909002250@gmail.com>
2023-03-29 19:51:59 +0800

    refactor: 排版Version物件

--- buildSrc/src/main/java/tw/idv/louislee/Version.kt
+++ buildSrc/src/main/java/tw/idv/louislee/Version.kt
@@ -3,14 +3,18 @@ package tw.idv.louislee
 object Version {
     const val SDK = 33
     const val MIN_SDK = 26
+
     const val KOTLIN_COMPIER_EXTENSION = "1.4.3"
     const val ANDROID_CORE = "1.9.0"
     const val ACTIVITY_COMPOSE = "1.6.1"
     const val COMPOSE = "1.3.3"
-    const val LIFECYCLE = "2.6.0"
     const val MATERIAL_3 = "1.1.0-alpha08"
+
+    const val LIFECYCLE = "2.6.0"
+
     const val KOIN = "3.3.3"
     const val KOIN_KSP = "1.1.1"
     const val KOIN_ANDROID_COMPOSE = "3.4.2"
+
     const val SQL_DELIGHT = "1.5.5"
 }
