758452175675ee09c127117d18a726afaac1fee1
Louis Li <a0909002250@gmail.com>
2023-04-26 21:05:02 +0800

    feat: 更新套件版本

--- buildSrc/src/main/java/tw/idv/louislee/Version.kt
+++ buildSrc/src/main/java/tw/idv/louislee/Version.kt
@@ -5,16 +5,16 @@ object Version {
     const val MIN_SDK = 26
 
     const val KOTLIN_COMPOSE_COMPILER_EXTENSION = "1.4.6"
-    const val ANDROID_CORE = "1.9.0"
-    const val ACTIVITY_COMPOSE = "1.6.1"
-    const val COMPOSE = "1.3.3"
-    const val MATERIAL_3 = "1.1.0-alpha08"
+    const val ANDROID_CORE = "1.10.0"
+    const val ACTIVITY_COMPOSE = "1.7.1"
+    const val COMPOSE = "1.4.2"
+    const val MATERIAL_3 = "1.1.0-rc01"
 
-    const val LIFECYCLE = "2.6.0"
+    const val LIFECYCLE = "2.6.1"
 
-    const val KOIN = "3.3.3"
-    const val KOIN_KSP = "1.1.1"
-    const val KOIN_ANDROID_COMPOSE = "3.4.2"
+    const val KOIN = "3.4.0"
+    const val KOIN_KSP = "1.2.0"
+    const val KOIN_ANDROID_COMPOSE = "3.4.4"
 
     const val SQL_DELIGHT = "1.5.5"
 
@@ -23,7 +23,7 @@ object Version {
     const val SHEETS_COMPOSE_DIALOGS = "1.1.1"
 
     const val CAMERA_X = "1.2.2"
-    const val CAMERA_X_ML_KIT_VISION = "1.3.0-alpha05"
+    const val CAMERA_X_ML_KIT_VISION = "1.3.0-alpha06"
 
     const val ML_KIT_BARCODE_SCANNING = "17.1.0"
     const val ML_KIT_BARCODE_SCANNING_COMMON = "17.0.0"
